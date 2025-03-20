63910f9059bcd82d433efd81cb464cc220dc2db2
Fernando Morales <20230032@ricaldone.edu.sv>
2025-03-20 10:00:11 -0600

    conexion y .env

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+import app from ".app.js";
+import "./database.js";
+import { config } from "./src/config.js";
+
+async function main() {
+    app.listen(config.server.PORT);
+    console.log("Server port " + config.server.PORT);
+}
