8394e7f4d2de571c706bc33880ebee7ba4b930a2
Fernando Morales <20230032@ricaldone.edu.sv>
2025-04-17 18:55:55 -0600

    Pagina de Contactanos

--- frontend/src/pages/contactanos.jsx
+++ frontend/src/pages/contactanos.jsx
@@ -1,6 +1,36 @@
-// contactanos.jsx
-export default function Contactanos() {
-    return (
-      <div>Contenido de contacto...</div>
-    );
-  }
+import React from 'react';
+import './css/contactanos.css';
+
+const Contactanos = () => {
+  return (
+    <div className="contact-form-container">
+      <h2 className="form-title">Ingresa tus datos</h2>
+      <form>
+        <input
+          type="text"
+          className="form-input"
+          placeholder="Nombre"
+        />
+        <input
+          type="text"
+          className="form-input"
+          placeholder="Apellido"
+        />
+        <input
+          type="email"
+          className="form-input"
+          placeholder="Email"
+        />
+        <textarea
+          className="form-input"
+          placeholder="Comentario"
+        ></textarea>
+        <button type="submit" className="form-button">
+          Enviar ahora
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Contactanos;
