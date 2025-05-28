e13aeb5795aa637b5806357a06b22040852f4a04
Kevin <kevinjah1975@gmail.com>
2025-05-27 19:09:15 -0600

    .

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -11,37 +11,37 @@
 import { Schema, model, ObjectId } from "mongoose";
 
 const userSchema = new Schema(
-  {
-    
-    firstName: {
-      type: String,
-      require: true,
-    },
+    {
 
-    lastName: {
-        type: String,
-        require: true,
-    },
+        firstName: {
+            type: String,
+            require: true,
+        },
 
-    email: {
-        type: String,
-        require: true,
-    },
+        lastName: {
+            type: String,
+            require: true,
+        },
 
-    password: {
-        type: String,
-        require: true,
-    },
+        email: {
+            type: String,
+            require: true,
+        },
 
-    profilePicture: {
-        type: String,
-        require: true,
-    },
+        password: {
+            type: String,
+            require: true,
+        },
 
-    privilages: {
-        type: String,
-        require: true,
-    },
-});
+        profilePicture: {
+            type: String,
+            require: true,
+        },
+
+        privilages: {
+            type: String,
+            require: true,
+        },
+    });
 
 export default model("User", userSchema);
